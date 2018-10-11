4a9eef163b9638d4ffe932a9b6e1a4c84f86f3d9
AnimationXin <18379177354@163.com>
2018-10-12 00:09:18 +0800

    feature: 封装elementUI组件。

---
+++ src/server/EPromise.js
@@ -0,0 +1,70 @@
+import { Loading, Message, MessageBox } from 'element-ui'
+
+
+function messageBox (propmt) {
+  return new Promise((resolve, reject) => {
+    MessageBox.confirm(`${propmt}`, '提示', {
+      confirmButtonText: '确定',
+      cancelButtonText: '取消',
+      type: 'warning'
+    }).then(() => {
+      resolve(true)
+    }).catch(() => {
+      reject(false)
+    })
+  })
+}
+
+function isSubmitEnable (form) {
+  return new Promise((resolve, reject) => {
+    form.validate((valid) => {
+      if (valid) {
+        resolve(true)
+      } else {
+        reject(false)
+      }
+    })
+  })
+}
+
+function showToast (msg, icons) {
+  Message({message: `${msg}`, type: `${icons}`})
+}
+
+function errMessage (msg) {
+  Message.error(`${msg}`)
+}
+
+function readFile (files) {
+  return new Promise((resolve, reject) => {
+    if (files.length) {
+      var file = files[0]
+      var reader = new window.FileReader()
+
+      reader.onload = function () {
+        resolve(reader.result)
+      }
+      reader.readAsText(file)
+    }
+  })
+}
+
+function validateEleForm (form) {
+  return new Promise((resolve, reject) => {
+    form.validate(valid => {
+      if (!valid) {
+        return reject()
+      }
+      resolve()
+    })
+  })
+}
+
+
+export default {
+  messageBox,
+  showToast,
+  errMessage,
+  isSubmitEnable,
+  validateEleForm
+}
