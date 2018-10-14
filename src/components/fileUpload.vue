11f8d31e7a1fbc98e58bf5c3b3ea530f7328e027
AnimationXin <18379177354@163.com>
2018-10-14 09:05:50 +0800

    feature: init头像上传组件

---
+++ src/components/fileUpload.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  computed: {
+  },
+  data () {
+    return {
+    }
+  },
+  mounted () {
+  },
+  created () {
+  }
+}
+</script>
+
+<style scopend>
+
+</style>
