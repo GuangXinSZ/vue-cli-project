cbafe20b046253b35eabe4bb009a85798df833ea
liugx <18379177354@163.com>
2018-10-23 18:00:54 +0800

    feature: init搜索组件。

---
+++ src/components/searchHistory.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="searchHistory">
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  computed: {
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  },
+  mounted () {
+  },
+  created () {
+  }
+}
+</script>
+
+<style scopend lang="scss">
+
+</style>
