266af536ceb3a338b6c20acb99d589d56f8ac03c
liugx <18379177354@163.com>
2018-10-31 23:29:33 +0800

    feature: 添加配置。

--- src/main.js
+++ src/main.js
@@ -12,6 +12,8 @@ import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
+// Vue.config.silent = true 取消Vue所有的日志与警告
+// Vue.config.devtools = true 配置是否允许检查代码 线上版本为false
 
 /* eslint-disable no-new */
 new Vue({
