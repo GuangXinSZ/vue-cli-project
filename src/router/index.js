63f85c59844ab294601ff87a29e11ded3839949c
liugx <18379177354@163.com>
2018-11-01 17:08:09 +0800

    feature: 登录。

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/view/home'
+import Login from '@/view/login'
 
 Vue.use(Router)
 
@@ -8,6 +9,11 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: 'Home',
       name: 'Home',
       component: Home
     }
---
+++ src/view/login/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="login">
+    <el-input v-model="from.user" placeholder="请输入内容"></el-input>
+    <el-input v-model="from.passWorld" placeholder="请输入内容"></el-input>
+    <el-button @click="loginAuth">登陆</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  computed: {
+  },
+  data () {
+    return {
+      from: {
+        user: '',
+        passWorld: ''
+      }
+    }
+  },
+  methods: {
+    loginAuth () {
+      if (this.from.user === '' || this.from.passWorld === '') {
+        this.$message.error('账号密码不能为空')
+        return
+      }
+      sessionStorage.setItem()
+    }
+  },
+  mounted () {
+  },
+  created () {
+  }
+}
+</script>
+
+<style scopend lang="scss">
+
+</style>
